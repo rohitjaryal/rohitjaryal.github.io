c442eaa311275edeaf5f0af35189000ca8669d2a
Rohit Jaryal <rohitjaryal@Ryans-MacBook-Pro.local>
2023-10-12 14:54:00 +0700

    feat: Cname added + ts fixes

--- src/views/Bio.tsx
+++ src/views/Bio.tsx
@@ -3,59 +3,11 @@
 import {
   Box,
   Container,
-  Divider,
   Heading,
-  HStack,
-  Image,
-  Tag,
   Text,
   useColorModeValue,
   VStack,
-  Wrap,
-  WrapItem,
 } from "@chakra-ui/react";
-
-interface Props {
-  marginTop?: number;
-  tags: any[];
-}
-
-const BlogTags = (props: Props) => {
-  const { marginTop = 0, tags } = props;
-
-  return (
-    <HStack spacing={2} marginTop={marginTop}>
-      {tags.map((tag) => {
-        return (
-          <Tag size={"md"} variant="solid" colorScheme="orange" key={tag}>
-            {tag}
-          </Tag>
-        );
-      })}
-    </HStack>
-  );
-};
-
-interface BlogAuthorProps {
-  date: Date;
-  name: string;
-}
-
-const BlogAuthor = (props: BlogAuthorProps) => {
-  return (
-    <HStack marginTop="2" spacing="2" display="flex" alignItems="center">
-      <Image
-        borderRadius="full"
-        boxSize="40px"
-        src="https://100k-faces.glitch.me/random-image"
-        alt={`Avatar of ${props.name}`}
-      />
-      <Text fontWeight="medium">{props.name}</Text>
-      <Text>—</Text>
-      <Text>{props.date.toLocaleDateString()}</Text>
-    </HStack>
-  );
-};
 
 const Bio = () => {
   return (
